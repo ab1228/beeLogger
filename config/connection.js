b71bfb3fbe2ddbdc951cf37428045e00612ccd6f
ab1228 <alexb8734@gmail.com>
2019-12-17 16:04:05 -0600

    connected db

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+var mysql = require('mysql');
+
+
+var connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DB
+});
+
+
+
+connection.connect(function (err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
+
+});
+
+module.exports = connection;
--- server.js
+++ server.js
@@ -1,1 +1,10 @@
 var express = require("express");
+
+var PORT = process.env.PORT || 8085;
+
+var app = express();
+
+
+app.listen(PORT, function () {
+    console.log("Server is listening on: http//localhost" + PORT);
+})
