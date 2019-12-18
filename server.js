441bdab09f761484bd99b8a35fa9cad3db8b95c0
ab1228 <alexb8734@gmail.com>
2019-12-18 14:02:32 -0600

    adde middelwared

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ var PORT = process.env.PORT || 8085;
 var app = express();
 
 
+app.use(express.static('public'));
+
+///middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
 app.listen(PORT, function () {
     console.log("Server is listening on: http//localhost" + PORT);
 })
