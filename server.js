3ab357aa77bd7800cc3b68e7aa70be9c58bbcd68
ab1228 <alexb8734@gmail.com>
2019-12-18 14:08:06 -0600

    set handlebars

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//set habndlebars
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view eignine', 'handlebars');
+
 
 app.listen(PORT, function () {
     console.log("Server is listening on: http//localhost" + PORT);
