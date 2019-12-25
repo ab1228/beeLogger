634890be0e36570c236192f03b44306f60a8a997
ab1228 <alexb8734@gmail.com>
2019-12-24 23:53:57 -0600

    added function to chnage bess from flying to landing

--- public/assets/js/bees.js
+++ public/assets/js/bees.js
@@ -1,3 +1,7 @@
 $(function () {
-    $()
+    $('change-flying').on('click', function (event) {
+        var id = $(this).data('id');
+        var newFlying = $(this).data('newlanding');
+
+    })
 })
